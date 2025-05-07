39b84003bf3f7bd31e1aa721fcb461cca17cac2b
remiandrieu <remi.andrieu@outlook.fr>
2025-05-07 17:27:24 +0200

    Draww some coins

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import pygame as pg
+import random as rd
 
 from assets.Coin import Coin
 
@@ -14,7 +15,12 @@ class Game:
         self.clock = pg.time.Clock()
         self.running = True
 
-        self.coin = Coin(10, 10)
+        self.coins = []
+        self.add_random_coins(3)
+    
+    def add_random_coins(self, n:int):
+        for _ in range(n):
+            self.coins.append(Coin(rd.randrange(self._length), rd.randrange(self._width)))
     
     def play(self):
 
@@ -26,7 +32,8 @@ class Game:
 
             self.screen.fill(self._background_color)
 
-            self.coin.draw(self.screen)
+            for coin in self.coins:
+                coin.draw(self.screen)
 
             pg.display.flip()
 
